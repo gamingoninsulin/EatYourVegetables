a300889eda2538dbba5d1be21b0cfec11cea3f43
GamingOnInsulin <gamingoninsulin@gmail.com>
2023-11-06 02:02:11 +0100

    updated stuf biome is now working

--- src/main/java/me/gamingoninsulin/eatyourvegtablesboymod/world/biome/ModOverworldRegion.java
+++ src/main/java/me/gamingoninsulin/eatyourvegtablesboymod/world/biome/ModOverworldRegion.java
@@ -16,7 +16,7 @@ public class ModOverworldRegion extends Region {
     public ModOverworldRegion(Identifier name, int weight) {
         super(name, RegionType.OVERWORLD, weight);
     }
-    
+
     @Override
     public void addBiomes(Registry<Biome> registry, Consumer<Pair<MultiNoiseUtil.NoiseHypercube, RegistryKey<Biome>>> mapper) {
         this.addModifiedVanillaOverworldBiomes(mapper, modifiedVanillaOverworldBuilder -> {
